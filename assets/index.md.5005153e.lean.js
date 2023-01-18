cfcfc4618617c5b07b1399c53e9c8960302c7e8d
bongxin <bongxin@yeah.net>
2023-01-18 10:50:30 +0000

    Deploying to gh-pages from  @ dae8d80ab002771b56c3d4d15aebab4aa960f1b7 🚀

---
+++ assets/index.md.5005153e.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as a,a as s}from"./app.a392a54c.js";const u=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"index.md","lastUpdated":1674038994000}'),o={name:"index.md"},n=s("p",null,"'# Hello VitePress'",-1),r=[n];function c(d,i,_,l,p,m){return a(),t("div",null,r)}const x=e(o,[["render",c]]);export{u as __pageData,x as default};
