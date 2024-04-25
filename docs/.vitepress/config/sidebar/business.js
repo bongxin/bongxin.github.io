87c48d1f660f1d4c05e2e48d2b70d386893f9dab
bongxin <bongxin@yeah.net>
2024-04-25 12:33:52 +0800

    招投标相关

---
+++ docs/.vitepress/config/sidebar/business.js
@@ -0,0 +1,11 @@
+export function sidebarFigma() {
+    return [
+      {
+        text: '招投标',
+        collapsed: false,
+        items: [
+          { text: '招投标', link: '招投标/认识招投标' }
+        ],
+      },
+    ];
+  }
